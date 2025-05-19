0c1a3256f6e41eb874d50aa7265569a78990f8bc
sarke <sarker4292@gmail.com>
2025-05-19 20:58:21 +0600

    add route Constants

---
+++ lib/app/constants/route_constants.dart
@@ -0,0 +1,9 @@
+class RouteConstants {
+  static const String splash = '/splash';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+  static const String addNote = '/add-note';
+// For editing, we might use a path parameter:
+// static const String editNote = '/edit-note/:noteId';
+}
